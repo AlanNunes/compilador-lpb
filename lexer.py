5515eb99c121502a67ad0c08eaeb823b613dcab7
Alan Nunes <alann.625@gmail.com>
2021-05-24 00:54:44 -0300

    Adiciona comentário ao código.

--- lexer.py
+++ lexer.py
@@ -6,6 +6,11 @@ import componentes_lexer.op_aritmetico as op_arit
 from componentes_lexer.posicao import Posicao
 from token_ import Token
 
+# O Lexer é responsável por ler todo o código-fonte
+#  e transformar cada pedaço em um Token que será
+#  utilizado no Parser para montar a árvore de sintáxe
+#  abstrata e realizar validações como análise sintática
+#  e análise semântica.
 class Lexer:
     _texto = ''
     _indiceAtual = 0
@@ -17,34 +22,44 @@ class Lexer:
         self._texto = texto
         self._carac_atual = texto[self._indiceAtual]
 
+    # Adiciona um token à lista de tokens gerados
+    #  durante a leitura do código fonte.
     def __adicionaToken(self, token: Token):
         self._tokens.append(token)
 
+    # Retorna o caracter atual a ser lido.
     def __retornaCaracterAtual(self, i = 0):
         if self._indiceAtual < len(self._texto):
             return  self._texto[self._indiceAtual + i]
         else:
             return None
 
+    # Retorna a posição atual de leitura.
+    # Exemplo: coluna 20 e linha 10.
     def __retornaPosicaoAtual(self) -> Posicao:
         return Posicao(self._linha, self._coluna)
 
+    # Avança um caracter (ou coluna).
     def __avancaColuna(self):
         c_atual = self.__retornaCaracterAtual() 
-        # Se encontrar uma quebra de linha, avança uma linha
+        # Se encontrar uma quebra de linha, avança uma linha.
         if c_atual == '\n':
             self.__avancaLinha()
         if len(self._texto) > self._indiceAtual:
             self._indiceAtual += 1
             self._coluna += 1
 
+    # Pula para a próxima linha do código fonte.
     def __avancaLinha(self):
         self._coluna = 0
         self._linha += 1
 
+    # Retorna verdadeiro se já tiver lido todo código fonte.
     def __EOF(self) -> bool:
         return self.__retornaCaracterAtual() == None
 
+    # Retorna um identificador.
+    # Exemplo: o nome de uma variável.
     def __retornaIdentificador(self) -> Token:
         valor = ''
         pos = self.__retornaPosicaoAtual()
@@ -56,6 +71,8 @@ class Lexer:
             return Token(p_chave, pos)
         return Token(tipos_tokens.identificador, pos, valor)
 
+    # Retorna um token de valor de texto
+    # Exemplo: 10
     def __retornaNumero(self) -> Token:
         valor = ''
         pos = self.__retornaPosicaoAtual()
@@ -68,6 +85,8 @@ class Lexer:
         except:
             return Token(valores.flutuante, pos, valor)
 
+    # Retorna um token de valor de texto
+    # Exemplo "Olá mundo!"
     def __retornaTexto(self) -> Token:
         valor = ''
         pos = self.__retornaPosicaoAtual()
@@ -79,6 +98,7 @@ class Lexer:
             self.__avancaColuna()
         return Token(tipo, pos, valor)
 
+    # Retorna um token do tipo operador relacional
     def __retornaOperadorRelacional(self) -> Token:
         pos = self.__retornaPosicaoAtual()
         operador = self.__retornaCaracterAtual()
@@ -99,6 +119,7 @@ class Lexer:
         elif operador == '!=':
             return Token(op_rel.desigualdade, pos)
 
+    # Retorna um token do tipo aritimético
     def __retornaOperadorAritmetico(self) -> Token:
         operador = ''
         pos = self.__retornaPosicaoAtual()
@@ -118,6 +139,8 @@ class Lexer:
         elif operador == ')':
             return Token(op_arit.parent_dir, pos)
 
+    # Realiza o 'tokenize' e retorna uma lista de tokens
+    #  gerados através do código fonte em lpb.
     def retornaTokens(self) -> list:
         while not self.__EOF():
             caracter_atual = self.__retornaCaracterAtual()
